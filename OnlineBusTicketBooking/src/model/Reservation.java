2abf5037bc098da07eb2659730fb4c8b5159d960
[sivaranjani28] <[sivaranjani.saro@gmail.com]>
2016-07-21 18:25:46 +0530

    [Adding model for User and Reservation]

---
+++ OnlineBusTicketBooking/src/model/Reservation.java
@@ -0,0 +1,86 @@
+package model;
+
+/**
+ * @author Ideas2IT-sivaranjani
+ *
+ */
+public class Reservation {
+    private int id;
+    private int userId;
+    private int busId;
+    private int tripId;
+    private int sourceId;
+    private int destinationId;
+    private int noOfSeats;
+    private int totalPrice;
+    private String paymentMode;
+    private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getBusId() {
+		return busId;
+	}
+	public void setBusId(int busId) {
+		this.busId = busId;
+	}
+	public int getTripId() {
+		return tripId;
+	}
+	public void setTripId(int tripId) {
+		this.tripId = tripId;
+	}
+	public int getSourceId() {
+		return sourceId;
+	}
+	public void setSourceId(int sourceId) {
+		this.sourceId = sourceId;
+	}
+	public int getDestinationId() {
+		return destinationId;
+	}
+	public void setDestinationId(int destinationId) {
+		this.destinationId = destinationId;
+	}
+	public int getNoOfSeats() {
+		return noOfSeats;
+	}
+	public void setNoOfSeats(int noOfSeats) {
+		this.noOfSeats = noOfSeats;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public String getPaymentMode() {
+		return paymentMode;
+	}
+	public void setPaymentMode(String paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", userId=" + userId + ", busId=" + busId + ", tripId=" + tripId
+				+ ", sourceId=" + sourceId + ", destinationId=" + destinationId + ", noOfSeats=" + noOfSeats
+				+ ", totalPrice=" + totalPrice + ", paymentMode=" + paymentMode + ", status=" + status + "]";
+	}
+    
+    
+}
