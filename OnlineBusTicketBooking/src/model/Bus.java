585f7c0c709503eaef4f19b44a4c5a594f9d22f5
[anupriya7295] <[anupriya7295@gmail.com]>
2016-07-22 11:20:28 +0530

    Added TimeStamp to Bus,City,Travels and Comments to respective classes

--- OnlineBusTicketBooking/src/model/Bus.java
+++ OnlineBusTicketBooking/src/model/Bus.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.sql.Time;
+
+/*
+ * Bus Model Object.
+ * <P>Contains various attributes of Bus and their getter setters.</p>
+ * 
+ * @author Anupriya-Ideas2It
+ * @version 1.0
+ */
 public class Bus {
 	private int id;
 	private String registrationNumber;
@@ -7,6 +16,10 @@ public class Bus {
 	private String  type;
 	private Boolean isAc;
 	private int totalNumberOfSeats;
+	private User createdBy;
+	private Time createdAt;
+	private User modifiedBy;
+	private Time modifiedAt; 
 	public int getId() {
 		return id;
 	}
@@ -43,10 +56,34 @@ public class Bus {
 	public void setTotalNumberOfSeats(int totalNumberOfSeats) {
 		this.totalNumberOfSeats = totalNumberOfSeats;
 	}
+	public User getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(User modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Time getModifiedAt() {
+		return modifiedAt;
+	}
+	public void setModifiedAt(Time modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+	public User getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Time getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Time createdAt) {
+		this.createdAt = createdAt;
+	}
 	@Override
 	public String toString() {
 		return "Bus [id=" + id + ", registrationNumber=" + registrationNumber + ", travelsId=" + travelsId + ", type="
 				+ type + ", isAc=" + isAc + ", totalNumberOfSeats=" + totalNumberOfSeats + "]";
 	}
-
+	
 }
